c275ce137fe096c341abc74f9d2ed46c98f8504e
C-Plusone <97445906+C-Plusone@users.noreply.github.com>
2022-01-19 01:25:57 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -16,3 +16,7 @@
 # Add a feed source
 echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default
 echo 'src-git small https://github.com/kenzok8/small' >>feeds.conf.default
+
+
+# 添加额外软件包
+# git clone https://github.com/P3TERX/xxx package/lean
